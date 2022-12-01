abd4db11530eaf8ab4d27c896e7013a341233edf
Vinicius Koji Enari <viniciusenari@gmail.com>
2022-12-01 20:54:36 -0300

    chapter 3.5

---
+++ projects/functions/src/main.rs
@@ -0,0 +1,27 @@
+// fn main() {
+//     print_labeled_measurement(5, 'h');
+// }
+
+// fn print_labeled_measurement(value: i32, unit_label: char) {
+//     println!("The measurement is: {value}{unit_label}");
+// }
+
+// fn five() -> i32 {
+//     5
+// }
+
+// fn main() {
+//     let x = five();
+
+//     println!("The value of x is: {x}");
+// }
+
+fn main() {
+    let x = plus_one(5);
+
+    println!("The value of x is: {x}");
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
+}
